3538c95a94771ddbb5cd43bcd016dec8497b7c75
Hyeonseok0627 <stonehead0627@gmail.com>
2023-10-26 11:49:25 +0900

    수업내용(231026) 231026 3교시 11 제트팩 액션바 오버플로우 메뉴 이벤트 추가 제트팩 검색 위젯 사용 + 이벤트 추가, 1)검색어 변경시 마다 호출 2) 검색어가 제출시 실행되는 함수 수정, 자동임포트로 패키지명 오류. import androidx.appcompat.widget.SearchView 고침

--- myapp_test_7_8_9_10_11_12/src/main/java/com/example/myapp_test_7_8_9_10_11_12/ch11_Test/Test11_ActionBarActivity.kt
+++ myapp_test_7_8_9_10_11_12/src/main/java/com/example/myapp_test_7_8_9_10_11_12/ch11_Test/Test11_ActionBarActivity.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.appcompat.widget.SearchView
 import com.example.myapp_test_7_8_9_10_11_12.R
 import com.example.myapp_test_7_8_9_10_11_12.databinding.ActivityTest11ActionBarBinding
 
@@ -34,6 +35,29 @@ class Test11_ActionBarActivity : AppCompatActivity() {
     // 액션바에 오버플로우 메뉴 붙이기
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+
+
+        // 검색 뷰에, 이벤트 추가하기.
+        val menuItem = menu?.findItem(R.id.menu_main_search)
+        // menuItem의 형을 SearchView 타입으로 변환, 형변환
+        //?:널 허용 연산
+        val searchView = menuItem?.actionView as SearchView
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            override fun onQueryTextChange(newText: String?): Boolean {
+                // 검색어가 변경 시 마다, 실행될 로직을 추가.
+                Log.d("lhs","텍스트 변경시 마다 호출 : ${newText}") // 문자열에서 데이터값 가져올 때 ${}사용
+                return  true
+            }
+
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                // 검색어가 제출이 되었을 경우, 연결할 로직.
+                // 사용자 DB에 검색을 하고, 그 결과 뷰를 출력하는 형태.
+                Toast.makeText(this@Test11_ActionBarActivity,"검색어가 전송됨 : ${query}",Toast.LENGTH_SHORT).show()
+                return true
+            }
+        })
+
         return super.onCreateOptionsMenu(menu)
     }
 
