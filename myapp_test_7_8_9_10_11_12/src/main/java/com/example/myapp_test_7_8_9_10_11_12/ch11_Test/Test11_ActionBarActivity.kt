4f5cd749aa7b722e1239d878aa907a65c4707067
Hyeonseok0627 <stonehead0627@gmail.com>
2023-10-26 11:20:53 +0900

    수업내용(231026) 231026 3교시 11 제트팩 액션바 오버플로우 메뉴 구성 11 제트팩 액션바 오버플로우 메뉴 이벤트 추가

--- myapp_test_7_8_9_10_11_12/src/main/java/com/example/myapp_test_7_8_9_10_11_12/ch11_Test/Test11_ActionBarActivity.kt
+++ myapp_test_7_8_9_10_11_12/src/main/java/com/example/myapp_test_7_8_9_10_11_12/ch11_Test/Test11_ActionBarActivity.kt
@@ -3,6 +3,9 @@ package com.example.myapp_test_7_8_9_10_11_12.ch11_Test
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import com.example.myapp_test_7_8_9_10_11_12.R
 import com.example.myapp_test_7_8_9_10_11_12.databinding.ActivityTest11ActionBarBinding
 
@@ -18,6 +21,7 @@ class Test11_ActionBarActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
 
+
         }
 
     // 액션바 클릭 이벤트 처리.
@@ -25,6 +29,31 @@ class Test11_ActionBarActivity : AppCompatActivity() {
         Log.d("lhs","test")
         onBackPressed() // 뒤로가기(이전화면으로 넘어가기)
         return super.onSupportNavigateUp()
-
     }
+
+    // 액션바에 오버플로우 메뉴 붙이기
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    // 오버플로우 메뉴 이벤트 핸들러 추가하기.
+    override fun onOptionsItemSelected(item: MenuItem): Boolean = when(item.itemId) {
+        0 -> {
+            Toast.makeText(this@Test11_ActionBarActivity,"메뉴1 클릭됨",Toast.LENGTH_SHORT).show()
+            true
+        }
+        1 -> {
+            Toast.makeText(this@Test11_ActionBarActivity,"메뉴2 클릭됨",Toast.LENGTH_SHORT).show()
+            true
+        }
+        2 -> {
+            Toast.makeText(this@Test11_ActionBarActivity,"메뉴3 클릭됨",Toast.LENGTH_SHORT).show()
+            true
+        }
+        // 람다식(화살표 함수("->"))에서 return 사용 못함.
+        else ->  super.onOptionsItemSelected(item)
+    }
+
+
 }
