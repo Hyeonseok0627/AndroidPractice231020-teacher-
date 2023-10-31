6e6d4a2113337f303a13d5ff65e2d9b4a23caa3e
Hyeonseok0627 <stonehead0627@gmail.com>
2023-10-31 13:41:16 +0900

    수업내용(231031) 231031 4교시 13 인텐트 이용, 데이터 게터 세터 기본 예제2, 뷰에 입력해서 넘기기

---
+++ myapp_test_7_8_9_10_11_12/src/main/java/com/example/myapp_test_7_8_9_10_11_12/ch13_Test/TestIntent2DetailActivity.kt
@@ -0,0 +1,29 @@
+package com.example.myapp_test_7_8_9_10_11_12.ch13_Test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myapp_test_7_8_9_10_11_12.R
+import com.example.myapp_test_7_8_9_10_11_12.databinding.ActivityTestIntent2DetailBinding
+import com.example.myapp_test_7_8_9_10_11_12.databinding.ActivityTestIntentBinding
+
+class TestIntent2DetailActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityTestIntent2DetailBinding
+    // 2번 화면
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityTestIntent2DetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 1번 화면에서 넘어온 데이터 받아서, 결과 뷰에 붙이기 작업.
+        // 임시 저장소에 담기
+        val name = intent.getStringExtra("name")
+        val email = intent.getStringExtra("email")
+        val age = intent.getIntExtra("age", 31)
+
+        binding.resulNametView.text = "이름 : $name"
+        binding.resulEmailtView.text = "이메일 : $email"
+        binding.resulAgetView.text = "나이 : $age"
+
+    }
+}
---
+++ myapp_test_7_8_9_10_11_12/src/main/java/com/example/myapp_test_7_8_9_10_11_12/ch13_Test/TestIntentActivity.kt
@@ -0,0 +1,42 @@
+package com.example.myapp_test_7_8_9_10_11_12.ch13_Test
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapp_test_7_8_9_10_11_12.databinding.ActivityTestIntentBinding
+
+class TestIntentActivity : AppCompatActivity() {
+    // 1번 화면
+    lateinit var binding : ActivityTestIntentBinding
+    var name : String = ""
+    var email : String = ""
+    var age : String = ""
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityTestIntentBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        name = binding.emailEdit.text.toString()
+        email = binding.passwordEdit.text.toString()
+        age = binding.ageEdit.text.toString()
+
+        // 인텐트에 기본 데이터 추가 및 가져오기 테스트.
+        binding.testBtn2.setOnClickListener {
+            // 데이터를 추가해서 + 화면 이동. : 내부 앱끼리 연동
+            val intent = Intent(this@TestIntentActivity,
+                TestIntent2DetailActivity::class.java)
+            // 데이터 추가 해보기.
+            intent.putExtra("name",name)
+            intent.putExtra("email",email)
+            intent.putExtra("age",age)
+            startActivity(intent)
+
+        }
+
+
+
+
+
+
+    }
+}
