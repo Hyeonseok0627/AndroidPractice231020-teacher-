538fe6767402fa47d23660483f32cba8645bd185
Hyeonseok0627 <stonehead0627@gmail.com>
2023-10-31 11:35:39 +0900

    수업내용(231031) 231031 2교시 탭 레이아웃 뼈대 작업4, 기본 출력 , 탭 뷰페이져2 연동3, 혼합뷰에 추가 최종 수정

--- myapp_test_7_8_9_10_11_12/src/main/java/com/example/myapp_test_7_8_9_10_11_12/ch11_Test/ViewTest2Activity.kt
+++ myapp_test_7_8_9_10_11_12/src/main/java/com/example/myapp_test_7_8_9_10_11_12/ch11_Test/ViewTest2Activity.kt
@@ -80,7 +80,7 @@ class ViewTest2Activity : AppCompatActivity() {
 
         // 뷰페이저2 프래그먼트 어댑터 이용해서 출력 해보기.
         // 현재 액티비티 도화지 , 캔버스 , 기본 화면.
-        // 여기에 뷰페이저와, 리사이클러뷰 2개를 붙이 예정.
+        // 여기에 뷰페이저와, 리사이클러뷰 2개를 붙일 예정.
 
         //뷰페이저2 준비물 1)프래그먼트 어댑터 2) 목록요소 , 프래그먼트 3개 필요. 3) 더미 데이터(=자기자신)
         binding.viewPager1.adapter = ViewPageAdapterTest(this)
