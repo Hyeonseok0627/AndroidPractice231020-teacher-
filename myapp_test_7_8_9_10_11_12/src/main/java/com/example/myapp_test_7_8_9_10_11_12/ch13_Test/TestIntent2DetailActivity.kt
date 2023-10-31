d338b696d0e71eb80cdf7ab1a6ea1bd6fce6396a
Hyeonseok0627 <stonehead0627@gmail.com>
2023-10-31 14:00:13 +0900

    수업내용(231031) 231031 5교시 13 인텐트 이용, 데이터 게터 세터 기본 예제2, 뷰에 입력해서 넘기기 수정작업중1

--- myapp_test_7_8_9_10_11_12/src/main/java/com/example/myapp_test_7_8_9_10_11_12/ch13_Test/TestIntent2DetailActivity.kt
+++ myapp_test_7_8_9_10_11_12/src/main/java/com/example/myapp_test_7_8_9_10_11_12/ch13_Test/TestIntent2DetailActivity.kt
@@ -2,6 +2,7 @@ package com.example.myapp_test_7_8_9_10_11_12.ch13_Test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.myapp_test_7_8_9_10_11_12.R
 import com.example.myapp_test_7_8_9_10_11_12.databinding.ActivityTestIntent2DetailBinding
 import com.example.myapp_test_7_8_9_10_11_12.databinding.ActivityTestIntentBinding
@@ -17,12 +18,16 @@ class TestIntent2DetailActivity : AppCompatActivity() {
 
         // 1번 화면에서 넘어온 데이터 받아서, 결과 뷰에 붙이기 작업.
         // 임시 저장소에 담기
-        val name = intent.getStringExtra("name")
         val email = intent.getStringExtra("email")
+        val password = intent.getStringExtra("password")
         val age = intent.getIntExtra("age", 31)
 
-        binding.resulNametView.text = "이름 : $name"
-        binding.resulEmailtView.text = "이메일 : $email"
+        // 데이터 확인
+        Log.d("lhs","데이터 확인 2번 화면 이메일: ${email}, "+
+                "패스워드: ${password}, 나이: ${age}")
+
+        binding.resulNametView.text = "이메일 : $email"
+        binding.resulEmailtView.text = "패스워드 : $password"
         binding.resulAgetView.text = "나이 : $age"
 
     }
